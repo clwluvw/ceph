80ad04fc3e831d9ca3847c95a6c0bf13fd628acc
Seena Fallah <seenafallah@gmail.com>
2024-11-13 19:36:42 +0100

    rgw: report last processed marker in datalog list
    With the introduction of log_zones, a zone might not receive certain
    datalog entries if they are irrelevant to that zone. To support
    proper trimming, we now return the last processed marker, allowing
    zones to report this marker even when they haven't processed those
    irrelevant entries. This ensures that the source zone can proceed
    with trimming.
    
    Signed-off-by: Seena Fallah <seenafallah@gmail.com>

--- src/rgw/driver/rados/rgw_datalog.h
+++ src/rgw/driver/rados/rgw_datalog.h
@@ -159,6 +159,23 @@ struct rgw_data_notify_entry {
   }
 };
 
+struct read_remote_data_log_last_marker {
+  uint32_t version = 2;
+  std::string log_id;
+  ceph::real_time log_timestamp;
+
+  void decode_json(JSONObj *obj) {
+    JSONDecoder::decode_json("log_id", log_id, obj);
+    utime_t ut;
+    JSONDecoder::decode_json("log_timestamp", ut, obj);
+    log_timestamp = ut.to_real_time();
+    JSONDecoder::decode_json("version", version, obj);
+  }
+};
+
+void encode_json(const char* name, const read_remote_data_log_last_marker& info,
+                 ceph::Formatter* f);
+
 class RGWDataChangesBE;
 
 class DataLogBackends final
@@ -187,8 +204,8 @@ public:
   }
   int list(const DoutPrefixProvider *dpp, int shard, int max_entries,
 	   std::vector<rgw_data_change_log_entry>& entries,
-	   std::string_view marker, std::string* out_marker, bool* truncated,
-	   optional_yield y, const std::string& rgwx_zonegroup);
+	   std::string_view marker, read_remote_data_log_last_marker *last_marker, bool* truncated,
+	   optional_yield y, const std::string& rgwx_zone);
   int trim_entries(const DoutPrefixProvider *dpp, int shard_id,
 		   std::string_view marker, optional_yield y);
   void trim_entries(const DoutPrefixProvider *dpp, int shard_id, std::string_view marker,
@@ -317,9 +334,9 @@ public:
   int get_log_shard_id(rgw_bucket& bucket, int shard_id);
   int list_entries(const DoutPrefixProvider *dpp, int shard, int max_entries,
 		   std::vector<rgw_data_change_log_entry>& entries,
-		   std::string_view marker, std::string* out_marker,
+		   std::string_view marker, read_remote_data_log_last_marker *last_marker,
 		   bool* truncated, optional_yield y,
-                   const std::string& rgwx_zonegroup);
+                   const std::string& rgwx_zone);
   int trim_entries(const DoutPrefixProvider *dpp, int shard_id,
 		   std::string_view marker, optional_yield y);
   int trim_entries(const DoutPrefixProvider *dpp, int shard_id, std::string_view marker,
@@ -332,7 +349,7 @@ public:
   int list_entries(const DoutPrefixProvider *dpp, int max_entries,
 		   std::vector<rgw_data_change_log_entry>& entries,
 		   LogMarker& marker, bool* ptruncated,
-		   optional_yield y, const std::string& rgwx_zonegroup);
+		   optional_yield y, const std::string& rgwx_zone);
 
   void mark_modified(int shard_id, const rgw_bucket_shard& bs, uint64_t gen);
   auto read_clear_modified() {
